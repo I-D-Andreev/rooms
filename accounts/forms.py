8a66137660ebee802e26f7412d1ef02cbd82f413
Ivan <iivvaann.andreev@gmail.com>
2020-11-21 22:06:58 +0000

    Set email to required

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,7 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
