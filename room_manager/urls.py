2a2428752231d1f594ca0147b00f89c5bd1e4498
Ivan <iivvaann.andreev@gmail.com>
2020-11-22 05:30:18 +0000

    Django default redirects to login

--- room_manager/urls.py
+++ room_manager/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 from room_manager.views import dashboard_view
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name="login")),
     path('dashboard/', dashboard_view, name="dashboard"),
 ]
