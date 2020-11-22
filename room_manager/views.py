f8669fc48c27ea4e59b26fef99c1a006054dae6b
Ivan <iivvaann.andreev@gmail.com>
2020-11-22 05:20:14 +0000

    Accounts to be redirected to their respective dashboards

--- room_manager/views.py
+++ room_manager/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from accounts.user_groups import UserGroups
+
 
 @login_required(login_url='login')
 def dashboard_view(request, *args, **kwargs):
-    return render(request, 'room_manager/dashboard.html')
+    dashboard = 'no_access'
+    if request.user.groups.exists():
+        group = request.user.groups.all()[0].name
+        if group == UserGroups.admins:
+            dashboard = 'admin'
+        elif group == UserGroups.users:
+            dashboard = 'user'
+        elif group == UserGroups.rooms:
+            dashboard = 'room'
+
+    return render(request, f'room_manager/{dashboard}_dashboard.html')
