f844695f968035030c8a24f2f0ee724dbe33c0f4
Ivan <iivvaann.andreev@gmail.com>
2020-11-23 21:42:23 +0000

    Add user groups back in (no need to save them in DB)

---
+++ accounts/user_groups.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class UserGroups:
+    admins = "admins"
+    users = "users"
+    rooms = "rooms"
+
+    @staticmethod
+    def as_list():
+        return [UserGroups.admins, UserGroups.users, UserGroups.rooms]
