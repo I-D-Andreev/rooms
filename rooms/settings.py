000507af16ca8bc5d74a64bf49c26439a34c9fae
Ivan <iivvaann.andreev@gmail.com>
2020-11-23 02:47:02 +0000

    Migrate to mysql database

--- rooms/settings.py
+++ rooms/settings.py
@@ -79,9 +79,15 @@ WSGI_APPLICATION = 'rooms.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'database.cnf')
+        }
     }
 }
 
