f8b18dbf1c726fa85d50a4c7f1220c4d401101f6
Ivan <iivvaann.andreev@gmail.com>
2020-11-24 23:29:05 +0000

    Profile toString method

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     public_name = models.CharField(max_length=255)
     type = models.CharField(max_length=255, choices=UserTypes.as_choice_list())
+
+    def __str__(self):
+        return self.public_name
