c601067049c764162735fcfb454afc93061d5ff6
Ivan <iivvaann.andreev@gmail.com>
2020-11-28 02:39:54 +0000

    Use Permission denied instead of returning error html

--- room_manager/views.py
+++ room_manager/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 
 
 @login_required(login_url='login')
 def dashboard_view(request, *args, **kwargs):
-    dashboard = 'no_access'
     try:
         dashboard = request.user.profile.type
     except ObjectDoesNotExist:
-        pass
+        raise PermissionDenied()
 
     return render(request, f'room_manager/{dashboard}_dashboard.html')
