6935d605f91e786e0455782e21ebaef264e0bdba
Ivan <iivvaann.andreev@gmail.com>
2021-01-17 17:29:31 +0000

    Add participants capacity to models

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     public_name = models.CharField(max_length=255)
     type = models.CharField(max_length=255, choices=UserTypes.as_choice_list())
+    capacity = models.IntegerField(default=0)
 
 # to add location and room-type
     def __str__(self):
