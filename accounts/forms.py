af39c2c390f094653fdfa3225c429d7b58be6cbd
Ivan <iivvaann.andreev@gmail.com>
2021-01-17 17:38:48 +0000

    Add capacity to rooms

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,7 @@ from accounts.models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
+    capacity = forms.IntegerField(min_value=0)
     type = forms.CharField(widget=forms.Select(choices=UserTypes.as_choice_list()))
 
     class Meta:
@@ -19,12 +20,15 @@ class UserRegistrationForm(UserCreationForm):
 
     def save(self, commit=True):
         cleaned_type = self.cleaned_data['type']
+        cleaned_capacity = self.cleaned_data.get('capacity', 0)
+
         account_type = cleaned_type if cleaned_type in UserTypes.as_list() else UserTypes.as_list()[1]
 
         user = super(UserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
-            Profile.objects.create(user=user, public_name=user.username, type=account_type)
+            Profile.objects.create(user=user, public_name=user.username, type=account_type, capacity=cleaned_capacity)
+
 
         return user
