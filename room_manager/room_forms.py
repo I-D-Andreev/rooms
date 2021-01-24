b12ae74b22dd2122a61f7e5ddcd627a0db60464f
Ivan <iivvaann.andreev@gmail.com>
2021-01-24 23:54:05 +0000

    Fix number input

--- room_manager/room_forms.py
+++ room_manager/room_forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class BookNowForm(forms.Form):
     meeting_name = forms.CharField(initial="Instant Booking", label="Meeting Name")
-    duration = forms.IntegerField(min_value=0, label="Duration (min)", widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))
+    duration = forms.IntegerField(min_value=0, label="Duration (min)", widget=forms.NumberInput(attrs={'autofocus': 'autofocus'}))
