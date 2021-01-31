747f655d1987c308d24ad7cff98b7b985f512bec
Ivan <iivvaann.andreev@gmail.com>
2021-01-31 21:53:00 +0000

    Display (basic) floors when a building is selected

--- room_manager/static/js/configure_floors.js
+++ room_manager/static/js/configure_floors.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#id_building').on('change', function(){
         buildingId = this.value;
         if(buildingId == ""){
-            renderFloorData([]);
+            renderFloorData(null);
         } else {
             $.ajax({
                 url: `/get-building-floors/${buildingId}`,
@@ -25,14 +25,24 @@ function renderFloorData(floors_list){
     // remove child elements
     floors_container.empty();
 
-    if(floors_list.length > 0){
-        for(let i=floors_list.length-1; i>=0; i--){
-            let element = `
-                <div>
-                    <h4> ${floors_list[i].name} </h4>
-                </div>
-            `;
+    if(floors_list !== null){
+        if(floors_list.length > 0){
+            for(let i=floors_list.length-1; i>=0; i--){
+                let element = `
+                    <div>
+                        <h4> ${floors_list[i].name} </h4>
+                    </div>
+                `;
 
+                floors_container.append(element);
+            }
+        }
+        else {
+            let element = `
+            <div>
+                <h4> No floors </h4>
+            </div>
+        `;
             floors_container.append(element);
         }
     }
