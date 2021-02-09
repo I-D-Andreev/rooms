fcd19197e68d01fb55be037fd9f5875d1b2f127b
Ivan <iivvaann.andreev@gmail.com>
2021-02-09 16:01:53 +0000

    Disable or enable fillable fieldswhen a room is chosen

--- room_manager/static/js/edit_room.js
+++ room_manager/static/js/edit_room.js
@@ -2,5 +2,31 @@ $(document).ready(function(){
     let buildingSelect = $('#id_building');
     let floorsSelect = $('#id_floor');
 
+
     loadFloorsOnBuildingChange(buildingSelect, floorsSelect);
+    loadRoomOnSelectChange();
+    disableFillableFields(true);
 })
+
+function loadRoomOnSelectChange(){
+    $('#id_room').on('change', function(){
+        let roomId = this.value;
+        if(roomId === ''){
+            disableFillableFields(true);
+        } else {
+            disableFillableFields(false);
+
+            
+        }
+    });
+
+}
+
+
+function disableFillableFields(isDisabled){
+    $('#id_public_name').attr('disabled', isDisabled);
+    $('#id_email').attr('disabled', isDisabled);
+    $('#id_capacity').attr('disabled', isDisabled);
+    $('#id_building').attr('disabled', isDisabled);
+    $('#id_floor').attr('disabled', isDisabled);
+}
