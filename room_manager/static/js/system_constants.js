127e5f4525aa78765cdbc65ff09e0a92af32b9a4
Ivan <iivvaann.andreev@gmail.com>
2021-02-14 15:15:08 +0000

    cancel button

--- room_manager/static/js/system_constants.js
+++ room_manager/static/js/system_constants.js
@@ -1,15 +1,31 @@
 var SHOULD_SHOW_ID = "Specific Number Of Floors";
+var INITIAL_DATA = null;
 
 $(document).ready(function(){
     let typeSelect = $("#id_type");
     showOrHideFloors(typeSelect.val() == SHOULD_SHOW_ID)
     
-    typeSelect.on('change', function(){
+    typeSelect.on("change", function(){
         let selectId = this.value;
         showOrHideFloors(selectId===SHOULD_SHOW_ID);
     });
-    
+
+    INITIAL_DATA = {
+        "type": typeSelect.val(),
+        "floors": $("#id_floors").val()
+    }
+
+    $("#cancel_button").on("click", cancelButtonClicked);
 });
+
+function cancelButtonClicked(){
+    let typeSelect = $("#id_type");
+    typeSelect.val(INITIAL_DATA.type);
+    $("id_floors").val(INITIAL_DATA.floors);
+
+    typeSelect.trigger("change");
+}
+
 
 function showOrHideFloors(shouldShow){
     
