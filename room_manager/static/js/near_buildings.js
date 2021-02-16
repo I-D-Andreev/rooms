26ebfb977860b23639f715dc6672a377b4dcb69e
Ivan <iivvaann.andreev@gmail.com>
2021-02-16 17:50:11 +0000

    Style the close proximity building pairs

--- room_manager/static/js/near_buildings.js
+++ room_manager/static/js/near_buildings.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     fadePythonMessages();
 })
+
+
+function crossClicked(building1Id, building2Id){
+    console.log(building1Id);
+    console.log(building2Id);
+
+    let divId = `${building1Id}_${building2Id}`;
+    console.log(divId);
+    // $(`#${divId}`).remove();
+
+}
