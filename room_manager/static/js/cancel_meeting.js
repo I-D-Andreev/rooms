91a6fe428b67b9e6006092c8c6a4375e9b2bd2e9
Ivan <iivvaann.andreev@gmail.com>
2021-02-23 19:56:33 +0000

    default js

---
+++ room_manager/static/js/cancel_meeting.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#id_meeting').on('change', function(){
+        mId = this.value;
+        if(mId === ''){
+            console.log("empty");
+        } else {
+            $.ajax({
+                url: `/get-meeting/${mId}`,
+                success: function(data){
+                    // loadMeeting(data.room, data.start_date, data.start_time, data.duration, data.participants_count);
+                    console.log(data);
+                },
+                error: function(err){
+                    console.log(err);
+                }
+            });
+        }
+    });
+});
+
+
+function loadMeeting(room, start_date, start_time, duration, participants_count){
+    // $('#id_room').val(room); 
+    // $('#id_start_date').val(start_date); 
+    // $('#id_start_time').val(start_time);
+    // $('#id_duration').val(duration);
+    // $('#id_participants_count').val(participants_count);
+}
