8a945c807a1e5d10b7f171a5e61e5f785cc7b941
Ivan <iivvaann.andreev@gmail.com>
2021-02-26 20:16:12 +0000

    Move statistics to its own folder

--- room_manager/user_statistics_views.py
+++ room_manager/user_statistics_views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # @login_required(login_url='login')
 # @user_only
 def statistics_view(request, *args, **kwargs):
-    return render(request, 'room_manager/user/statistics.html')
+    return render(request, 'room_manager/user/statistics/statistics.html')
