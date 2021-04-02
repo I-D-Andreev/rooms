b8ea98588ff2d6ec7ede145768836eaa885b049e
Ivan <iivvaann.andreev@gmail.com>
2021-04-02 22:07:54 +0100

    Three charts

--- room_manager/static/js/statistics/room_utilization.js
+++ room_manager/static/js/statistics/room_utilization.js
@@ -1,11 +1,13 @@
 var chart_daily = null;
 var chart_weekly = null;
+var chart_monthly = null;
 
 $(document).ready(function(){
     const DAILY_UTILIZATION = JSON.parse(document.getElementById("daily_util").textContent);
     const DAYS = DAILY_UTILIZATION['days'];
     let CHART_DAILY_ID = 'chart_daily';
     let CHART_WEEKLY_ID = 'chart_weekly';
+    let CHART_MONTHLY_ID = 'chart_monthly';
 
     $('#id_room').on('change', function(){
         let roomId = this.value;
@@ -19,6 +21,10 @@ $(document).ready(function(){
                 chart_weekly.destroy();
             }
 
+            if (chart_monthly !== null){
+                chart_monthly.destroy();
+            }
+
         }
         else {
             // do stuff
@@ -27,6 +33,7 @@ $(document).ready(function(){
             if(data) {
                 chart_daily = createPercentageChart(CHART_DAILY_ID, DAYS, `${roomName} Daily Percentage Utilization`, data);
                 chart_weekly = createPercentageChart(CHART_WEEKLY_ID, DAYS, `${roomName} Weekly Percentage Utilization`, data);
+                chart_monthly = createPercentageChart(CHART_MONTHLY_ID, DAYS, `${roomName} Monthly Percentage Utilization`, data);
             }
         }
     });
