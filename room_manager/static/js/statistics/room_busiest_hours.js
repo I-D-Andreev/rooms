6a953d4231efa370acefb7f41a8d5d1c7a37604c
Ivan <iivvaann.andreev@gmail.com>
2021-04-03 23:49:21 +0100

    Rename Weekly to Last 7 Days

--- room_manager/static/js/statistics/room_busiest_hours.js
+++ room_manager/static/js/statistics/room_busiest_hours.js
@@ -10,14 +10,13 @@ $(document).ready(function(){
     const BUSIEST_DAILY = JSON.parse(document.getElementById("busy_hours_daily").textContent);
     const BUSIEST_WEEKLY = JSON.parse(document.getElementById("busy_hours_weekly").textContent);
     
-    
     if (BUSIEST_DAILY) {
         createBusiestHoursChart(CHART_DAILY_ID, BUSIEST_DAILY.hours, "Busiest Hours Today", BUSIEST_DAILY.hours_count);
     }
 
 
     if (BUSIEST_WEEKLY) {
-        createBusiestHoursChart(CHART_WEEKLY_ID, BUSIEST_WEEKLY.hours, "Busiest Hours This Week", BUSIEST_WEEKLY.hours_count);
+        createBusiestHoursChart(CHART_WEEKLY_ID, BUSIEST_WEEKLY.hours, "Busiest Hours Last 7 Days", BUSIEST_WEEKLY.hours_count);
     }
 
 });
