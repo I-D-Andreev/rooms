ccd4495d3b48782d32e0d72f24b784bbd989b7d3
Ivan <iivvaann.andreev@gmail.com>
2021-04-06 18:55:47 +0100

    fix edit account focus

--- room_manager/static/js/edit_account.js
+++ room_manager/static/js/edit_account.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     let buildingSelect = $('#id_building');
     let floorsSelect = $('#id_floor');
-
+    
     loadFloorsOnBuildingChange(buildingSelect, floorsSelect);
+    
+    $("#id_old_password").removeAttr("autofocus");
+    floorsSelect.focus();
 })
