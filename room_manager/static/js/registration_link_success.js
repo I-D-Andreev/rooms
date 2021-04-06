8eb603fd21f13ce15fce74323447b9fbd4e1eefd
Ivan <iivvaann.andreev@gmail.com>
2021-04-06 21:13:40 +0100

    remove aconsole log

--- room_manager/static/js/registration_link_success.js
+++ room_manager/static/js/registration_link_success.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 
     $("#copy_icon").on('click', function(){
-        console.log($("#register_url").val());
         $("#register_url").select();
         document.execCommand('copy');
 
