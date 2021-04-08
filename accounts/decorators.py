bd3ca90cfde727f74d2846957270ad7d8d346286
Ivan <iivvaann.andreev@gmail.com>
2021-04-08 22:53:46 +0100

    Revert the if statement logic of decorators

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -11,6 +11,17 @@ def unauthenticated_user_only(view_function):
     return wrapper
 
 
+def admin_or_user_only(view_function):
+    def wrapper(request, *args, **kwargs):
+        if (hasattr(request.user, 'profile')) and \
+                (request.user.profile.type == UserTypes.admin or request.user.profile.type == UserTypes.user):
+            return view_function(request, *args, **kwargs)
+        else:
+            return redirect('dashboard')
+       
+    return wrapper
+
+
 def admin_only(view_function):
     return certain_account_type_only(view_function, UserTypes.admin)
 
@@ -25,9 +36,9 @@ def user_only(view_function):
 
 def certain_account_type_only(view_function, acc_type):
     def wrapper(request, *args, **kwargs):
-        if (not hasattr(request.user, 'profile')) or (request.user.profile.type != acc_type):
-            return redirect('dashboard')
-        else:
+        if (hasattr(request.user, 'profile')) and (request.user.profile.type == acc_type):
             return view_function(request, *args, **kwargs)
+        else:
+            return redirect('dashboard')
 
     return wrapper
