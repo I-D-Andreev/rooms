de5a1c788f80d9c6a657ffebef33a49a7fa415a9
Ivan <iivvaann.andreev@gmail.com>
2021-04-09 01:33:18 +0100

    check is_valid before saving

--- accounts/forms.py
+++ accounts/forms.py
@@ -21,21 +21,24 @@ class UserRegistrationForm(UserCreationForm):
         self.fields['email'].required = True
 
     def save(self, commit=True):
-        cleaned_type = self.cleaned_data['type']
-        cleaned_capacity = self.cleaned_data.get('capacity')
+        if self.is_valid():
+            cleaned_type = self.cleaned_data['type']
+            cleaned_capacity = self.cleaned_data.get('capacity')
 
-        account_type = cleaned_type if cleaned_type in UserTypes.as_list() else UserTypes.user
+            account_type = cleaned_type if cleaned_type in UserTypes.as_list() else UserTypes.user
 
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        if commit:
-            try:
-                with transaction.atomic():
-                    user.save()
-                    Profile.objects.create(user=user, public_name=user.username, type=account_type, capacity=cleaned_capacity)
-            
-            except Exception as e:
-                print(f"Error: {e}")
-                return None
+            user = super(UserCreationForm, self).save(commit=False)
+            user.set_password(self.cleaned_data['password1'])
+            if commit:
+                try:
+                    with transaction.atomic():
+                        user.save()
+                        Profile.objects.create(user=user, public_name=user.username, type=account_type, capacity=cleaned_capacity)
+                
+                except Exception as e:
+                    print(f"Error: {e}")
+                    return None
 
-        return user
+            return user
+        else:
+            return None
