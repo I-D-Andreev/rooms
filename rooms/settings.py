529096eb00f53cc6b5b689264d24a0d509e66583
Ivan <iivvaann.andreev@gmail.com>
2021-04-19 22:08:31 +0100

    settings py comment

--- rooms/settings.py
+++ rooms/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript'
+    'livereload.middleware.LiveReloadScript' # comment out to get rid of livereload.js error
 ]
 
 ROOT_URLCONF = 'rooms.urls'
